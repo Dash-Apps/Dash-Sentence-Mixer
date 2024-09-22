7e8aeaed598546bdc76b0de6335a29743ee91191
stylo-codes-stuff <mistah6909@gmail.com>
2024-09-22 22:29:05 +0000

    finished functions and added audio and video files so users can experiment with the software.

--- functions.py
+++ functions.py
@@ -4,34 +4,50 @@ import pathlib
 from pydub import AudioSegment
 from faster_whisper import WhisperModel
 supported_filetypes = [".m4a",".mp3",".webm",".mp4",".mpga",".wav",".mpeg",]
+#custom exception for when a file is opened with an unsupported filetype.
 class UnsupportedFileType(Exception):
     def __init__(self, message):            
         super().__init__(message)
             
 class time_stamp:
-    def __init__(self,start,end,word,confidence):
-        self.start = start *1000 - 50
-        self.end = end * 1000 +50
+    '''Class to store data for each timestamp.'''
+    def __init__(self,start,end,word,confidence,bias):
+        self.start = start * 1000 #the start and end are multiplied into milliseconds to be compatible with pydub
+        self.end = end * 1000  + bias 
         self.word = word
         self.confidence = round(confidence,2)
-def get_timestamps(audio):
+
+def get_timestamps(audio,bias):
+    """Creates and returns a list of timestamps for the given audio file."""
+    #supported filetype check
     if get_filetype(audio) not in supported_filetypes:
         raise UnsupportedFileType(f"Audio file given uses unsupported file format {pathlib.Path(audio).suffix}")
+    #load whisper
     model = WhisperModel('small')
-    segments, _ = model.transcribe(audio, word_timestamps=True)
+    segments, _ = model.transcribe(audio, word_timestamps="word")
     words = []
+    #Create a timestamp for each segment using the timestamp class and add it to a list.
     for segment in segments:
         for word in segment.words:
-            words.append(time_stamp(word.start,word.end,word.word,word.probability))
+            words.append(time_stamp(word.start,word.end,word.word,word.probability,bias))
+    #return the list
     return words
-def get_segments(audio):
-    timestamps = get_timestamps(audio)
-    for timestamp in timestamps:
-        segment = AudioSegment.from_file(audio,get_filetype(audio).replace(".",""))
-        slice = segment[timestamp.start:timestamp.end]
-        slice.export(f"./segments/{timestamp.word} {timestamp.confidence}.mp3", format="mp3")
 
-        
+def get_segments(audio,file_format,bias):
+    '''Creates files for the given audio in the provided file format'''
+
+    #Open the given file with pydub
+    segment = AudioSegment.from_file(audio,get_filetype(audio).replace(".",""))
+    #create a list of timestamps
+    timestamps = get_timestamps(audio,bias)
+    #go through each timestamp in the list
+    for timestamp in timestamps:
+        #Slice and export each timestamp.
+        slice = segment[timestamp.start:timestamp.end]
+        slice.export(f"./segments/{timestamp.word} {timestamp.confidence}.{file_format}", format=f"{file_format}")
+
+
+
 def get_filetype(name):
     return pathlib.Path(name).suffix
-get_segments("narration.mp3")
+get_segments("tests/Link _ The Faces of Evil - Intro (English) (HD 1080i).mp4","mp4",200)
